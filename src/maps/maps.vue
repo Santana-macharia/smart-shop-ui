fd22dd5a79acc178ad09c9f8996b2bfcaa82202f
Santana-macharia <santanamash@gmail.com>
2019-03-21 13:07:00 +0300

    first commit

---
+++ src/maps/maps.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+
+    <div id="myMap"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Disease Outbreak Prediction using Data Analytics Map'
+    }},
+  mounted: function() {
+        console.log("map: ", google.maps)
+            this.map = new google.maps.Map(document.getElementById('myMap'), {
+            center: {lat:61.180059, lng: -149.822075},
+            scrollwheel: false,
+            zoom: 4
+            })
+  }
+
+}
+</script>
+<style scoped>
+    #myMap {
+    height:300px;
+    width: 100%;
+   }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router';
 import Login from '../auth/Login';
 import Main from '../site/Main';
 import site_routes from '../site/routes';
+import maps from '../maps/maps';
 
 
 
@@ -18,6 +19,12 @@ export default new Router({
       component: Login,
     },
     {
+      path: '/maps',
+      name: 'maps',
+      component: maps,
+    },
+    {
+
       path: '/',
       component: Main,
       children: site_routes,
